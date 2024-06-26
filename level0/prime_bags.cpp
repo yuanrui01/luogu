c1d4f18731a01851a09c4f709c231651c5f0a8e3
yuanrui01 <xdyrfree@gmail.com>
2024-06-27 01:58:16 +0800

    P5723 【深基4.例13】质数口袋

---
+++ level0/prime_bags.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int l;
+    cin >> l;
+
+    int sum = 0;
+    int cnt = 0;
+
+    for (int i = 2; i < 100001; ++i) {
+        bool is_prime = true;
+        int j = 2;
+        for (int j = 2; j * j <= i; ++j) {
+            if (i % j == 0)
+                is_prime = false;
+        }
+        if (is_prime) {
+            if (sum + i > l)
+                break;
+            else {
+                cout << i << endl;
+                sum += i;
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
