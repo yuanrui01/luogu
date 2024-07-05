19da0a6d1e5486d58f39bebb87d1d623475e556c
yuanrui <xdyrfree@gmail.com>
2024-07-05 17:43:39 +0800

    补充

--- level0/shell_sort.cpp
+++ level0/shell_sort.cpp
@@ -8,14 +8,13 @@ int main() {
     vector<int> a = {9,8,7,6,5,4,3,2,1};
     int sz = a.size();
     int h = 1;
-    while (h < sz/3)
-        h = 3*h + 1;
+    while (h < sz / 3)
+        h = 3 * h + 1;
     while (h >= 1) {
-        for (int i = h; i < sz; ++i) {
-            for (int j = i; j >= h && a[j] < a[j - h]; j -= h) {
+        // h = 1时即插入排序逻辑
+        for (int i = h; i < sz; ++i) 
+            for (int j = i; j >= h && a[j] < a[j - h]; j -= h) 
                 swap(a[j], a[j - h]);
-            }
-        }
         h /= 3;
     }
     for (int num : a)
