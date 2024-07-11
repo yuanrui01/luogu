7f572e11fff04738a7255801a1a8863fbebe985d
yuanrui <xdyrfree@gmail.com>
2024-07-11 11:31:32 +0800

    自底向上归并排序

--- level0/merge_sort.cpp
+++ level0/merge_sort.cpp
@@ -30,6 +30,9 @@ void merge(int a[], int low, int mid, int high) {
 
 /**
  * 自顶向下归并排序
+ * 可扩展的优化点：
+ * 1. 对于小数组直接使用插入排序
+ * 2. merge之前检查 mid和mid+1元素，如果满足 nums[mid] <= nums[mid + 1] 说明已经有序，无需merge
  */
 void sort(int a[], int low, int high) {
     if (high <= low)
@@ -37,12 +40,26 @@ void sort(int a[], int low, int high) {
     int mid = low + (high - low) / 2;
     sort(a, low, mid);
     sort(a, mid + 1, high);
+    if (mid + 1 <= high && a[mid] <= a[mid + 1])
+        return;
     merge(a, low, mid, high);
+}
+
+/**
+ * 自底向上归并排序
+ */
+void upper_sort(int a[]) {
+    int N = 9;
+    for (int sz = 1; sz < N; sz += sz) {
+        for (int lo = 0; lo < N - sz; lo += (sz + sz)) {
+            merge(a, lo, lo + sz - 1, min(lo + sz + sz - 1, N - 1));
+        }
+    }
 }
 
 int main(void) {
     int a[] = {9,8,7,6,5,4,3,2,1};
-    sort(a, 0, 8);
+    upper_sort(a);
     for (int i = 0; i < 9; ++i)
         cout << a[i] << " ";
     cout << endl;
